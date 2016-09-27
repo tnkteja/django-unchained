2127cb53a28f4c3c940c735e9653686585ed4d6a
Neela Krishna Teja Tadikonda <tnkteja@gmail.com>
2016-09-27 10:51:28 -0400

    .

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -215,5 +215,5 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
